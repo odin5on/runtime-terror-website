75d00f023790a13a2ef75d7f41a3a4d801784e60
Nina <nbaffo@uiowa.edu>
2023-11-28 15:47:41 -0600

    deleted css file dont need that

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -4,9 +4,20 @@ function Nina() {
   return (
     <div>
       <h1>Hello from Nina!</h1>
-      <img src = "src/pages/IMG_3307.jpeg" alt = "My puppy!"/>
-      <img src = "src/pages/IMG_5860.jpeg" alt = "Me with my mom and sisters."/>
-      <img src = "src/pages/IMG_8281.jpeg" alt = "The Bahamas"/>
+      <div style = {{display: "flex"}}>
+        <img src = "src/pages/IMG_3307.jpeg" 
+             alt = "My puppy!"
+             style = {{width: "150px", marginRight: "10px"}}
+        />
+      <img src = "src/pages/IMG_5860.jpeg" 
+           alt = "Me with my mom and sisters."
+           style = {{width: "150px", marginRight: "10px"}}
+        />
+      <img src = "src/pages/IMG_8281.jpeg"
+           alt = "The Bahamas"
+           style = {{width: "150px"}}
+        />
+      </div>
     </div>
   );
 }
