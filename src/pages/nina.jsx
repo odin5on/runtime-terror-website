e218ac9dec32cd77ab91e3e1ee4d646a7527d6c0
Nina <nbaffo@uiowa.edu>
2023-11-28 16:34:15 -0600

    made smaller

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Nina() {
   return (
     <div>
-      <h1 style = {{ fontWeight: "bold", fontSize: "50px"}}> Nina Baffo</h1>
+      <h1 style = {{ fontWeight: "bold", fontSize: "24px"}}> Nina Baffo</h1>
 
       <div style={{ display: "flex", alignItems: "center" }}>
       <img src = "src/pages/Screen Shot 2023-11-10 at 9.51.00 AM.png"
            style = {{width: "250px"}}
         />
-      <p style={{ marginLeft: "1cm", marginBottom: "3cm", fontWeight: "bold", fontSize: "40px"}}>About me:</p>
+      <p style={{ marginLeft: "1cm", marginBottom: "3cm", fontWeight: "bold", fontSize: "20px"}}>About me:</p>
       </div>
       <div style = {{marginTop: "2in", display: "flex", felxDirection: "column", alighnItems: "flex-end", paddingLeft: "2in"}}>
         <img src = "src/pages/IMG_3307.jpeg" 
