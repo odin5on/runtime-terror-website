7109ba78a408874c58774d6de65a7b5037b47f52
Nina <nbaffo@uiowa.edu>
2023-11-28 16:22:28 -0600

    adding paragraph about myself

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -4,9 +4,13 @@ function Nina() {
   return (
     <div>
       <h1>Hello from Nina!</h1>
+
+      <div style={{ display: "flex", alignItems: "center" }}>
       <img src = "src/pages/Screen Shot 2023-11-10 at 9.51.00 AM.png"
            style = {{width: "250px"}}
         />
+      <p style={{ marginLeft: "1cm" }}>" here is a little bit about me"</p>
+      </div>
       <div style = {{marginTop: "2in", display: "flex", felxDirection: "column", alighnItems: "flex-end", paddingLeft: "2in"}}>
         <img src = "src/pages/IMG_3307.jpeg" 
              alt = "My puppy!"
