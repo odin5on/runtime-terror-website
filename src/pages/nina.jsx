1230286278564006be3c792fd52058f1f563855a
Nina <nbaffo@uiowa.edu>
2023-11-28 15:54:03 -0600

    changed the way the pictures are laid out

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 function Nina() {
   return (
-    <div>
+    <div style = {{display: "flex", felxDirection: "column", alighnItems: "flex-end", paddingRight: "2in"}}>
       <h1>Hello from Nina!</h1>
-      <div style = {{display: "flex"}}>
+      <div style = {{marginBottom: "1cm"}}>
         <img src = "src/pages/IMG_3307.jpeg" 
              alt = "My puppy!"
-             style = {{width: "300px", marginRight: "10px"}}
+             style = {{width: "300px", marginRight: "0.5cm"}}
         />
       <img src = "src/pages/IMG_5860.jpeg" 
            alt = "Me with my mom and sisters."
-           style = {{width: "300px", marginRight: "10px"}}
+           style = {{width: "300px", marginRight: "0.5cm"}}
         />
       <img src = "src/pages/IMG_8281.jpeg"
            alt = "The Bahamas"
-           style = {{width: "450px"}}
+           style = {{width: "500px"}}
         />
       </div>
     </div>
