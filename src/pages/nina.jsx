2a6d70925b69e53c496802e40ebb8a7ab3cd0d40
Nina <nbaffo@uiowa.edu>
2023-11-28 15:49:40 -0600

    made pictures bigger

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -7,15 +7,15 @@ function Nina() {
       <div style = {{display: "flex"}}>
         <img src = "src/pages/IMG_3307.jpeg" 
              alt = "My puppy!"
-             style = {{width: "150px", marginRight: "10px"}}
+             style = {{width: "300px", marginRight: "10px"}}
         />
       <img src = "src/pages/IMG_5860.jpeg" 
            alt = "Me with my mom and sisters."
-           style = {{width: "150px", marginRight: "10px"}}
+           style = {{width: "300px", marginRight: "10px"}}
         />
       <img src = "src/pages/IMG_8281.jpeg"
            alt = "The Bahamas"
-           style = {{width: "150px"}}
+           style = {{width: "450px"}}
         />
       </div>
     </div>
