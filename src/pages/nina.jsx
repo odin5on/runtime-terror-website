81da1ef27176c0f84a6bb507d49f5530f750ed0b
Nina <nbaffo@uiowa.edu>
2023-11-28 18:58:30 -0600

    ?

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -17,13 +17,14 @@ function Nina() {
       
       <div style = {{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center" }}>
         <img src = "src/pages/IMG_3307.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "3cm"}} />
+        <p style={{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center",fontSize: "17px", display: "flex" }}>This is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.</p>
+        
         <img src = "src/pages/IMG_5860.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "3cm"}}/>
+        <p style={{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center",fontSize: "17px", display: "flex" }}>Pictured here is me, my mom, and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.</p>
+        
         <img src = "src/pages/IMG_8281.jpeg" style = {{width: "450px", marginBottom: "3cm"}}/>
+        <p style={{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center",fontSize: "17px", display: "flex" }}>This is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. </p>
       </div>
-        <p style={{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center",fontSize: "17px", display: "flex" }}>Pictured above are a few of my favorite things to summarize a little more about myself. To the left is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.
-          The center picture is me, my mom and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.
-          To the right is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. 
-        </p>
     </div>
   );
 }
