91f614b9f0d6f3216e4d2afcc7d4271d962afca5
Nina <nbaffo@uiowa.edu>
2023-11-29 13:27:35 -0600

    okay added import statements

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import bahamas from "./assets/bahamas.jpeg";
+import family from "./assets/family.jpeg";
+import puppy from "./assets/puppy.jpeg";
+import myPic from "./assets/profilePic.png";
 
 function Nina() {
   return (
     <div>
       <div style={{ backgroundColor: "lightblue", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginRight: "1cm", display: "flex", alignItems: "center" }}>
       <p style={{ fontWeight: "bold", fontSize: "25px",  marginBottom: "300px"}}>Nina Baffo</p>
-        <img src = {"src/assets/profilePic.png"} style = {{width: "250px"}}/>
+        <img src = {myPic} style = {{width: "250px"}}/>
  
         <p style={{ marginLeft: "1cm", fontWeight: "bold", fontSize: "17px"}}>About me:</p>
 
@@ -16,17 +20,17 @@ function Nina() {
       
       <div style = {{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginRight: "1cm", display: "flex", alignItems: "center" }}>
        <div style={{ marginRight: "1cm", marginLeft: "1cm"}}>
-        <img src = {"/src/assets/puppy.jpeg"} style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
+        <img src = {puppy} style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
           <p style={{ fontSize: "17px" }}>This is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.</p>
         </div>
 
         <div style={{ marginRight: "1cm", marginLeft: "1cm" }}>
-        <img src = {"/src/assets/family.jpeg"} style = {{width: "300px", marginRight: "1cm", marginBottom: "0.5cm"}}/>
+        <img src = {family} style = {{width: "300px", marginRight: "1cm", marginBottom: "0.5cm"}}/>
           <p style={{ fontSize: "17px" }}>Pictured here is me, my mom, and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.</p>
         </div>
         
         <div style={{ marginRight: "1cm", marginLeft: "1cm" }}>
-        <img src = {"/src/assets/bahamas.jpeg"} style = {{width: "800px", marginRight: "1cm", marginBottom: "1cm"}}/>
+        <img src = {bahamas} style = {{width: "800px", marginRight: "1cm", marginBottom: "1cm"}}/>
           <p style={{ fontSize: "17px" }}>This is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. </p>
         </div>
       </div>
