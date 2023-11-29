e476883357a6c22cc338bdbbd95e60e3ffa0fa27
Nina <nbaffo@uiowa.edu>
2023-11-28 18:51:03 -0600

    display

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -6,24 +6,23 @@ function Nina() {
       <h1 style = {{fontWeight: "bold", fontSize: "24px"}}> Nina Baffo</h1>
 
       <div style={{ backgroundColor: "lightblue", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center" }}>
-      <img src = "src/pages/Screen Shot 2023-11-10 at 9.51.00 AM.png"
-           style = {{width: "250px"}}/>
+        <img src = "src/pages/Screen Shot 2023-11-10 at 9.51.00 AM.png" style = {{width: "250px"}}/>
  
-      <p style={{ marginLeft: "1cm", marginBottom: "3cm", fontWeight: "bold", fontSize: "17px"}}>About me:</p>
+        <p style={{ marginLeft: "1cm", marginBottom: "3cm", fontWeight: "bold", fontSize: "17px"}}>About me:</p>
 
-      <p style={{ backgroundColor: "lightpink", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginBottom: "1.5cm", fontSize: "17px"}}>Hi and welcome to my page! My name is Nina, I'm from the southwest suburbs of chicago. I am a senior this year majoring in Computer Science and Engineering.
-      I started off college as a Math Education major, but switched to Engineering second semester my sophmore year. I was nervous at first, but I am really glad I decided to push myself and pursue something out of my comfort zone. 
-      I can't wait to see where my career takes me in the near future! </p>
+        <p style={{ backgroundColor: "lightpink", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginBottom: "1.5cm", fontSize: "17px"}}>Hi and welcome to my page! My name is Nina, I'm from the southwest suburbs of chicago. I am a senior this year majoring in Computer Science and Engineering.
+          I started off college as a Math Education major, but switched to Engineering second semester my sophmore year. I was nervous at first, but I am really glad I decided to push myself and pursue something out of my comfort zone. 
+          I can't wait to see where my career takes me in the near future! </p>
       </div>
       
       <div style = {{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center" }}>
         <img src = "src/pages/IMG_3307.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "3cm"}} />
         <img src = "src/pages/IMG_5860.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "3cm"}}/>
         <img src = "src/pages/IMG_8281.jpeg" style = {{width: "450px", marginBottom: "3cm"}}/>
-        <p style={{ fontSize: "17px" }}>Pictured above are a few of my favorite things to summarize a little more about myself. To the left is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.
-     The center picture is me, my mom and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.
-     To the right is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. 
-</p>
+        <p style={{ fontSize: "17px", display: "flex", alignItems: "center" }}>Pictured above are a few of my favorite things to summarize a little more about myself. To the left is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.
+          The center picture is me, my mom and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.
+          To the right is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. 
+        </p>
       </div>
     </div>
   );
