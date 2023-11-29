7c76fa0b8bb5dd05f55fa64661cc9e6384f18f5c
Addie Schroeder <adrienne-schroeder@uiowa.edu>
2023-11-29 11:29:56 -0600

    Completed Webpgae

--- src/pages/addie.jsx
+++ src/pages/addie.jsx
@@ -4,14 +4,19 @@ import "./Addie.css";
 import image1 from './addie_pics/kev.jpeg';
 import image2 from './addie_pics/roomies.jpeg';
 import image3 from './addie_pics/dance.jpeg';
+import profilePic from './addie_pics/cover_pic.png';
 
 function Addie() {
   return (
-    <div>
+    <div style={{backgroundColor:"lightgrey"}}>
+      <div style={{backgroundColor:"lightblue"}}>
+        <h1 class="text-3xl text-center font-bold"><br></br> Addie Schroeder <br></br></h1>
+      </div>
       <div className="split-container">
-        <div class="font-sans md:font-serif text-center w-full h-20" className="left-column margin">
-          
-            <h1 class="text-3xl font-bold text-center">Addie Schroeder</h1>
+        <div className="left-column margin">
+          <div>
+            <img src={profilePic} alt="Profile Pic" style={{width: "250px"}}/>
+          </div>
             
         </div>
 
@@ -19,9 +24,9 @@ function Addie() {
           <p>
             <h2 class="font-bold">A little about myself:</h2>
 
-            <p class="text-l" className="right-column paragraph"> 
+            <p class="text-l" className="right-column paragraph" style={{backgroundColor:"lightBlue"}}> 
 
-              I am a dedicated student at the University of Iowa, majoring in computer 
+              Hi! My name is Addie. I am a dedicated student at the University of Iowa, majoring in computer 
               science and engineering while also pursuing a minor in mathematics. In addition 
               to my academic pursuits, I spent the last four years representing the university 
               as a member of the Iowa dance team. Outside of my studies, I am passionate about 
@@ -36,9 +41,9 @@ function Addie() {
         </div>
       </div>
 
-      <div className="image-row" style={{backgroundColor: "white"}} >
+      <div className="image-row" style={{backgroundColor: "lightGrey"}} >
         {/* Position images */}
-        <img src={image1} alt="Kev" className="image"/>
+        <img src={image1} alt="Kev" className="image border"/>
         <img src={image2} alt="Roomies" className="image" />
         <img src={image3} alt="Dance" className="image" />
       </div>
