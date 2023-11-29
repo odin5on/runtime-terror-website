db6cad1209daf4455ad3e986e816f68ae15992e6
Nina <nbaffo@uiowa.edu>
2023-11-29 10:40:28 -0600

    okay think i got it

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -8,7 +8,7 @@ function Nina() {
   return (
     <div>
       <div style={{ backgroundColor: "lightblue", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginRight: "1cm", display: "flex", alignItems: "center" }}>
-        <img src = {profilePic} style = {{width: "250px"}}/>
+        <img profilePic = {profilePic} style = {{width: "250px"}}/>
  
         <p style={{ marginLeft: "1cm", marginBottom: "3cm", fontWeight: "bold", fontSize: "17px"}}>About me:</p>
 
@@ -19,17 +19,17 @@ function Nina() {
       
       <div style = {{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginRight: "1cm", display: "flex", alignItems: "center" }}>
        <div style={{ marginRight: "1cm", marginLeft: "1cm"}}>
-        <img src = {puppy} style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
+        <img puppy = {puppy} style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
           <p style={{ fontSize: "17px" }}>This is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.</p>
         </div>
 
         <div style={{ marginRight: "1cm", marginLeft: "1cm" }}>
-        <img src = {family} style = {{width: "300px", marginRight: "1cm", marginBottom: "0.5cm"}}/>
+        <img family = {family} style = {{width: "300px", marginRight: "1cm", marginBottom: "0.5cm"}}/>
           <p style={{ fontSize: "17px" }}>Pictured here is me, my mom, and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.</p>
         </div>
         
         <div style={{ marginRight: "1cm", marginLeft: "1cm" }}>
-        <img src = {bahamas} style = {{width: "800px", marginRight: "1cm", marginBottom: "1cm"}}/>
+        <img bahamas = {bahamas} style = {{width: "800px", marginRight: "1cm", marginBottom: "1cm"}}/>
           <p style={{ fontSize: "17px" }}>This is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. </p>
         </div>
       </div>
