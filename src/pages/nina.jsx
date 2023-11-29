348f83bf7eeb4f0228c39f7e7e6af6104ee1f0b6
Nina <nbaffo@uiowa.edu>
2023-11-29 10:48:01 -0600

    pics

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import puppy from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/puppy.jpeg";
-import profilePic from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/profilePic.png";
-import family from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/family.jpeg";
-import bahamas from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/bahamas.jpeg";
+import puppyPic from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/puppy.jpeg";
+import profile from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/profilePic.png";
+import familyPic from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/family.jpeg";
+import bahamasPic from "/Users/ninabaffo/runtime-terror-website/src/assets/NinasPics/bahamas.jpeg";
 
 function Nina() {
   return (
     <div>
       <div style={{ backgroundColor: "lightblue", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginRight: "1cm", display: "flex", alignItems: "center" }}>
-        <img profilePic = {profilePic} style = {{width: "250px"}}/>
+        <img profilePic = {profile} style = {{width: "250px"}}/>
  
         <p style={{ marginLeft: "1cm", fontWeight: "bold", fontSize: "17px"}}>About me:</p>
 
@@ -19,17 +19,17 @@ function Nina() {
       
       <div style = {{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", marginRight: "1cm", display: "flex", alignItems: "center" }}>
        <div style={{ marginRight: "1cm", marginLeft: "1cm"}}>
-        <img puppy = {puppy} style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
+        <img puppy = {puppyPic} style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
           <p style={{ fontSize: "17px" }}>This is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.</p>
         </div>
 
         <div style={{ marginRight: "1cm", marginLeft: "1cm" }}>
-        <img family = {family} style = {{width: "300px", marginRight: "1cm", marginBottom: "0.5cm"}}/>
+        <img family = {familyPic} style = {{width: "300px", marginRight: "1cm", marginBottom: "0.5cm"}}/>
           <p style={{ fontSize: "17px" }}>Pictured here is me, my mom, and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.</p>
         </div>
         
         <div style={{ marginRight: "1cm", marginLeft: "1cm" }}>
-        <img bahamas = {bahamas} style = {{width: "800px", marginRight: "1cm", marginBottom: "1cm"}}/>
+        <img bahamas = {bahamasPic} style = {{width: "800px", marginRight: "1cm", marginBottom: "1cm"}}/>
           <p style={{ fontSize: "17px" }}>This is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. </p>
         </div>
       </div>
