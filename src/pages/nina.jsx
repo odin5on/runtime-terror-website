5d9ef3402b50d65982318f4097297a6cc6d0f4f1
Nina <nbaffo@uiowa.edu>
2023-11-28 19:12:57 -0600

    yay done

--- src/pages/nina.jsx
+++ src/pages/nina.jsx
@@ -17,17 +17,17 @@ function Nina() {
       
       <div style = {{ backgroundColor: "lavender", padding: "10px", borderRadius: "5px", marginBottom: "10px", marginLeft: "1cm", display: "flex", alignItems: "center" }}>
        <div style={{ marginRight: "1cm" }}>
-        <img src = "src/pages/IMG_3307.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "2cm"}} />
+        <img src = "src/pages/IMG_3307.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}} />
           <p style={{ fontSize: "17px" }}>This is my puppy, his name is Benny and he just turned 8 months old. He is the best dog in the world, I take him with me everywhere.</p>
         </div>
 
         <div style={{ marginRight: "1cm" }}>
-        <img src = "src/pages/IMG_5860.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "2cm"}}/>
+        <img src = "src/pages/IMG_5860.jpeg" style = {{width: "300px", marginRight: "1cm", marginBottom: "1cm"}}/>
           <p style={{ fontSize: "17px" }}>Pictured here is me, my mom, and sisters at an Iowa game this year. They are my best friends and I would do anything for them. I love my family very much.</p>
         </div>
         
         <div style={{ marginRight: "1cm" }}>
-        <img src = "src/pages/IMG_8281.jpeg" style = {{width: "450px", marginBottom: "2cm"}}/>
+        <img src = "src/pages/IMG_8281.jpeg" style = {{width: "450px", marginBottom: "1cm"}}/>
           <p style={{ fontSize: "17px" }}>This is a picture of when I went to the Bahamas. I love traveling to new places and expanding my knowledge about different areas of the world. </p>
         </div>
       </div>
