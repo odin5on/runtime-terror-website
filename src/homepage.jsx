8828f320bc11ae969de1670d82eaaa77eadbf776
Addie Schroeder <adrienne-schroeder@uiowa.edu>
2023-12-01 00:04:36 -0600

    Homepage Pic and modifcations to my page

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,6 +1,7 @@
 import ContactCard from "./components/contactcard";
 
 import dannypicture from "./assets/danny.jpg";
+import addiepicture from "./pages/addie_pics/cover_pic.png"
 import sid from "./assets/sid.jpg";
 
 function HomePage() {
@@ -9,7 +10,7 @@ function HomePage() {
       <div className="grid grid-cols-2 gap-8 justify-center justify-items-center">
         <ContactCard name="Danny Bodin" picture={dannypicture} to="/danny" />
         <ContactCard name="Jordin Eicher" picture={sid} to="/jordin" />
-        <ContactCard name="Addie Schroeder" picture={sid} to="/addie" />
+        <ContactCard name="Addie Schroeder" picture={addiepicture} to="/addie" />
         <ContactCard name="Nina Baffo" picture={sid} to="/nina" />
       </div>
     </div>
